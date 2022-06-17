aaef4119672007b9b6ac4fad0a1ef91e3ef3b970
Nikolay <jump9207@gmail.com>
2022-06-17 15:42:20 +0300

    correct style

--- views/CommentView.jsx
+++ views/CommentView.jsx
@@ -6,7 +6,7 @@ function CommentView({
   return (
     <div className="card">
       <div className="itemIncoment1">{user.user_name}</div>
-      <div>{commentRes.content}</div>
+      <div className="commentText">{commentRes.content}</div>
       <div className="itemIncoment2">{` ${new Date(commentRes.createdAt).getDate()}.${new Date(commentRes.createdAt).getMonth()}.${new Date(commentRes.createdAt).getFullYear()} ${new Date(commentRes.createdAt).getHours()}:${new Date(commentRes.createdAt).getMinutes()}`}</div>
     </div>
   );

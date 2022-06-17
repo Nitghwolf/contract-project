63d6c747b05dd22954f2df1cde6d5b57a9fb6374
Андрей Волков <nitghwolf13@gmail.com>
2022-06-17 12:24:20 +0300

    add message for error

--- routes/register.js
+++ routes/register.js
@@ -18,7 +18,7 @@ router.post('/', async (req, res) => {
     const { username, password } = req.body;
     const existingUser = await User.findOne({ where: { user_name: username } });
     if (existingUser) {
-      res.renderErrorComponent();
+      res.renderErrorComponent({ message: 'Такой пользователь уже существует'});
       return;
     }
 
@@ -50,7 +50,7 @@ router.post('/login', async (req, res) => {
       req.session.userId = existingUser.id;
       res.redirect('/');
     } else {
-      res.renderErrorComponent();
+      res.renderErrorComponent({ message: 'Неверный логин или пароль, попробуйте авторизоваться заново'});
     }
   } catch (error) {
     res.renderErrorComponent();
